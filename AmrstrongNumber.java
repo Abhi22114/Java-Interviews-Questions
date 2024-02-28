3d0047d43042353135446bd4b9240af6e4151b65
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-28 18:31:40 +0530

    Create AmrstrongNumber.java

---
+++ AmrstrongNumber.java
@@ -0,0 +1,39 @@
+Armstrong number
+
+Write a Java program that asks the user to enter the integer N and then determines if that number is an Armstrong number.
+
+Armstrong number: Armstrong number is the number in any given number base, which forms the total of the same number when each of its digits is raised to the power of the number of digits in the number. For example, if you have a number like 153, it's an Armstrong number because 1 + 5^3 + 3^3 equals 153.
+
+Input format:
+
+The input is a positive integer.
+
+Output format:
+
+The output states whether the given integer is an Armstrong number, as shown in displayed test cases.
+
+Sample Test Cases
+
+Test Case 1:
+
+Expected Output:
+
+153
+
+153 is an Armstrong number
+
+Test Case 2:
+
+Expected Output:
+
+202
+
+202 is not an Armstrong number
+
+Finish
+
+esc
+
+F1
+
+F2
