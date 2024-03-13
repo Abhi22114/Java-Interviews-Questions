8a13f6bf91794cd46f69bc720e63f02400621605
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-13 23:23:55 +0530

    Create Beverageclass.java

---
+++ Beverageclass.java
@@ -0,0 +1,41 @@
+Create a class Beverage with a method order Beverage() that prints "Ordering a beverage"
+
+Create a subclass Coffee that extends Beverage. Override the orderBeverage() method to prompt the user for input. Ask the user to input the coffee type (String), e.g., latte, cappuccino, and size (String), e.g., Small, Large, Medium, and then print the ordered coffee details.
+
+Create another subclass Tea that extends Beverage. Override the order Beverage() method to prompt the user for input. Ask the user to input the tea type (String), e.g., green tea, chamomile, and sweetness level (String), then print the ordered tea details.
+
+Note: The main class has been provided to you in the editor.
+
+Sample Test Cases
+
+Test Case 1:
+
+Expected Output:
+
+Ordering a beverage
+
+Coffee type: Latte
+
+size of the coffee: Large
+
+Ordered Coffee Details:
+
+Coffee Type: Latte
+
+Size: Large
+
+Tea type: Green Tea
+
+Sweetness level: Mildly sweet
+
+Ordered Tea Details:
+
+Tea Type: Green Tea
+
+Eweetness Level: Mildly sweet
+
+Test Case 2:
+
+
+
+
