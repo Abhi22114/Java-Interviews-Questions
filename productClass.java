ea2ee268e4fe3a5832d8075331bc3d40918e4dca
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-14 00:36:49 +0530

    Update productClass.java

--- productClass.java
+++ productClass.java
@@ -1,3 +1,58 @@
+Mouing vanavie prouucuvame.
+
+• An int variable quantity.
+
+• A method inputProductDetails() that takes user input for the product name.
+
+• A method displayQuantity() that prints the current quantity.
+
+Create a class named Purchase that extends Product with the following attributes:
+
+An int variable purchaseQuantity.
+
+• A method input Purchase Details() that takes user input for the quantity purchased.
+
+Create a new method updatelnventory() in the Purchase class that updates the quantity based on the purchase.
+
+Note: The main class has been provided to you in the editor.
+
+Sample Test Cases
+
+Test Case 1:
+
+Expected Output:
+
+Enter product name: Smart Phone
+
+Current Quantity: 0
+
+Enter quantity purchased: 12
+
+Inventory Updated Successfully!
+
+Current Quantity: 12
+
+Test Case 2:
+
+Expected Output:
+
+Enter product name: Electric lights
+
+Current Quantity: 0
+
+Enter quantity purchased: 25
+
+Inventory Updated Successfully!
+
+Current Quantity: 25
+
+Finish
+
+
+
+
+
+    
 import java.util.Scanner;
 
 class Product {
