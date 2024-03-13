45ccafe16282a738d022ffe2925761cdcb2529f3
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-14 00:01:23 +0530

    Create inheritenceclass.java

---
+++ inheritenceclass.java
@@ -0,0 +1,44 @@
+
+
+Person's name and age - Single Inheritance
+
+Create a class named Person with the following characteristics:
+
+• A String variable name.
+
+A method inputName() that takes user input for the name.
+
+A method displayName() that prints the name to the console.
+
+Create a class named Citizen that inherits from Person and has the following characteristics:
+
+• An int variable age.
+
+• A method inputAge() that takes user input for the age.
+
+• A method displayAge() that prints the age to the console.
+
+Note: The main class has been provided to you in the editor. The MainPerson class creates an instance of Citizen, takes user input for name and age, and then displays the entered name and age. The program demonstrates the use of inheritance, where the Citizen class inherits attributes and methods from the Person class.
+
+Sample Test Cases
+
+Test Case 1:
+
+Expected Output:
+
+Enter name: John doe
+
+Name: John doe
+
+Enter age: 23
+
+Age: 23
+
+Test Case 2:
+
+Expected Output:
+
+Enter name: Shreya
+
+Name: Shreya
+
