4d8170ebaf972be579947fd04df287df832ed08f
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-03-13 23:45:15 +0530

    Create Payrollsystem.java

---
+++ Payrollsystem.java
@@ -0,0 +1,45 @@
+Payroll system
+
+You are designing a payroll system for a company.
+
+Create a base class Employee with attributes name (String), employeeld (int), and salary (double). Implement a method calculateSalary that returns the salary.
+
+Create a child class Manager that inherits from Employee and adds an attribute bonus (double). Implement a method calculateSalary in the Manager class that overrides the base class method to include the bonus in the salary calculation.
+
+Note: The main class has been provided to you in the editor.
+
+Ex--->>
+
+Sample Test Cases
+
+Test Case 1:
+
+Expected Output:
+
+Enter details for an employee:
+
+Name: John
+
+Employee ID: 101
+
+Salary: $50000
+
+Enter details for a manager:
+
+Bonus: $ 10000
+
+Employee Details:
+
+Name: John
+
+Employee ID: 101
+
+Bane Salary: $50000.0
+
+Manager Details:
+
+Bonus: 510000.0
+
+Total Salary: $60000.0
+
+
